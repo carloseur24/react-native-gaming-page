2654eec6b0e7d678aa4d9a69b04caeaacdb6de81
carlos <carloseur24@gmail.com>
2025-02-17 22:17:40 -0400

    feat: ✨Implement Main component to fetch and display latest games, refactor App to use SafeAreaProvider and encapsulate game list logic

---
+++ app/Main.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { StyleSheet, View, Image, Text, ScrollView } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { getLatestGames, GameType } from "../lib/metacritic";
+
+export default function Main() {
+  const [games, setGames] = useState<GameType[]>([]);
+  const insets = useSafeAreaInsets();
+
+  
+  useEffect(() => {
+    getLatestGames().then((games: GameType[]) => setGames(games));
+  }, []);
+
+
+  return (
+    <>
+      <ScrollView>
+        {games.map((game, index) => (
+          <View key={game.id || index} style={styles.card}>
+            <Image source={{ uri: game.image }} style={styles.image} />
+            <Text style={styles.title}>{game.name}</Text>
+            <Text style={styles.score}>{game.topCriticScore?.toFixed(2)}</Text>
+            <Text style={styles.description}>{game.description}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    alignItems: "baseline",
+    justifyContent: "center",
+    backgroundColor: "#000",
+    marginBottom: 20, // Add some spacing between cards
+  },
+  image: {
+    width: 200,
+    height: 300,
+    resizeMode: "cover", // Use resizeMode instead
+    borderRadius: 10,
+    marginBottom: 15,
+  },
+  title: {
+    fontWeight: "bold",
+    color: "#eee",
+    fontSize: 20,
+  },
+  description: {
+    marginTop: 10,
+    fontWeight: "500",
+    color: "#eee",
+    fontSize: 18,
+  },
+  score: {
+    marginTop: 10,
+    backgroundColor: "#0e06",
+    fontWeight: "bold",
+    color: "#eee",
+    fontSize: 18,
+    paddingVertical: 5, // Use paddingVertical and paddingHorizontal
+    paddingHorizontal: 8,
+    borderRadius: 10,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,17 @@
-import { useState, useEffect } from "react";
 import {
-  StatusBar,
   StyleSheet,
   View,
-  Image,
-  Text,
-  ScrollView,
 } from "react-native";
-import { getLatestGames, GameType } from "../lib/metacritic";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Main from "./Main";
 
 export default function App() {
-  const [games, setGames] = useState<GameType[]>([]);
-
-  useEffect(() => {
-    getLatestGames().then((games: GameType[]) => setGames(games));
-  }, []);
-  console.log(games);
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
-      <SafeAreaView style={styles.safeArea}>
-        <Text style={styles.title}>App React Native 2</Text>
-        <ScrollView style={styles.scrollViewContent}>
-          {games.map((game, index) => (
-            <View key={game.id || index} style={styles.card}>
-              <Image source={{ uri: game.image }} style={styles.image} />
-              <Text style={styles.title}>{game.name}</Text>
-              <Text style={styles.score}>
-                {game.topCriticScore?.toFixed(2)}
-              </Text>
-            </View>
-          ))}
-        </ScrollView>
-      </SafeAreaView>
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -42,36 +19,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#000",
-  },
-  safeArea: {
-    flex: 1,
-    margin: 12,
-  },
-  scrollViewContent: {},
-  card: {
-    alignItems: "baseline",
-    justifyContent: "center",
-    backgroundColor: "#000",
-    marginBottom: 10, // Add some spacing between cards
-  },
-  image: {
-    width: 200,
-    height: 300,
-    resizeMode: "contain", // Use resizeMode instead
-    borderRadius: 10,
-  },
-  title: {
-    fontWeight: "bold",
-    color: "#eee",
-    fontSize: 20,
-  },
-  score: {
-    backgroundColor: "#0e06",
-    fontWeight: "bold",
-    color: "#eee",
-    fontSize: 18,
-    paddingVertical: 5, // Use paddingVertical and paddingHorizontal
-    paddingHorizontal: 8,
-    borderRadius: 10,
+    padding: 12
   },
 });

a01fa2999f238635d7934a14bdfaa1b636b33019
carlos <carloseur24@gmail.com>
2025-02-23 21:20:26 -0400

    feat: 🎨 Add Logo and update Main component with logo rendering

---
+++ app/components/Main.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { View, ActivityIndicator, FlatList } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { getLatestGames, GameType } from "../../lib/metacritic";
+import GameCard from "./GameCard";
+import { Logo } from "./Logo";
+
+export default function Main() {
+  const [games, setGames] = useState<GameType[]>([]);
+  const insets = useSafeAreaInsets();
+
+  useEffect(() => {
+    getLatestGames().then((games: GameType[]) => setGames(games));
+  }, []);
+
+  return (
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom, paddingHorizontal: 12 }}>
+      <Logo />
+      {games.length === 0 ? (
+        <View style={{ display: "flex", width: "100%", height: "100%", justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator color="#fff" size="large" />
+        </View>
+        ) : (
+          <FlatList
+            data={games}
+            renderItem={({ item }) => <GameCard game={item} />}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        )
+      }
+    </View>
+  );
+}
+

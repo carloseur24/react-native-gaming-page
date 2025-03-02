91e4aa51732672bb1ae610ec280dc280d06eefd8
carlos <carloseur24@gmail.com>
2025-03-02 09:15:11 -0400

    refactor: 🎨 Enhance Icons component with prop spreading
    - Updated ReturnIcon and AboutIcon to accept additional props
    - Improved flexibility by using prop spreading
    - Maintained default icon styling while allowing customization

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,9 +1,23 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 
-export const ReturnIcon = () => {
-  return <Ionicons name="return-down-back-sharp" size={24} color="white" />;
+export const ReturnIcon = (props: any) => {
+  return (
+    <Ionicons
+      name="return-down-back-sharp"
+      size={24}
+      color="white"
+      {...props}
+    />
+  );
 };
 
-export const AboutIcon = () => {
-  return <Ionicons name="information-circle-outline" size={24} color="white" />;
+export const AboutIcon = (props: any) => {
+  return (
+    <Ionicons
+      name="information-circle-outline"
+      size={24}
+      color="white"
+      {...props}
+    />
+  );
 };

40fdcd806d212610e9c7dac359a3391c404ad04d
carlos <carloseur24@gmail.com>
2025-03-02 08:58:56 -0400

    feat (add): 🎨 Create Icons component with ReturnIcon and AboutIcon
    - Added new Icons.tsx file in components directory
    - Implemented ReturnIcon and AboutIcon using Ionicons
    - Exported icons for reusability across the application

---
+++ components/Icons.tsx
@@ -0,0 +1,9 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export const ReturnIcon = () => {
+  return <Ionicons name="return-down-back-sharp" size={24} color="white" />;
+};
+
+export const AboutIcon = () => {
+  return <Ionicons name="information-circle-outline" size={24} color="white" />;
+};

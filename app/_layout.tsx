d84fca8003e133aace7cd1f852281bd26783281c
carlos <carloseur24@gmail.com>
2025-03-04 22:46:06 -0400

    feat: 🚀 Implement root layout with Stack navigation and header
    - Added Stack navigation in root layout
    - Configured header with Logo and AboutIcon
    - Replaced Slot with Stack for more flexible routing
    - Added header styling and navigation link to About page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,26 @@
-import { Slot } from "expo-router";
-import { View } from "react-native";
+import Logo from "@/assets/svg/Logo";
+import { AboutIcon } from "@/components/Icons";
+import { Link, Stack } from "expo-router";
+import { Pressable, View } from "react-native";
 
 export default function RootLayout() {
   return (
-    <View className="flex-1 bg-black items-center justify-center">
-      <Slot />
+    <View className="flex-1">
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: "black" },
+          headerTintColor: "white",
+          headerTitle: "",
+          headerLeft: () => <Logo width={200} height={80} />,
+          headerRight: () => (
+            <Link href="/about" asChild>
+              <Pressable className="active:opacity-50" hitSlop={20}>
+                <AboutIcon size={35} />
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
     </View>
   );
 }
